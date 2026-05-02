1f1d063742769b6bfa9948f566357b991f68c241
Sahil Pohare <sahilpohare@gmail.com>
2026-05-02 15:56:23 +0100

    Set production URL and baseUrl to site root

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,10 +11,9 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://sahilpohare.github.io',
+  url: 'https://docs.pingerchips.com',
   // Set the /<baseUrl>/ pathname under which your site is served
-  // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/pingerchips-docs/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
